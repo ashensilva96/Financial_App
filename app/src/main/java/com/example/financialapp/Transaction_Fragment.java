4c97407c942801feb35358d20c5f686a80bdba1b
Ashen Silva <ashensilva96@gmail.com>
2022-06-23 22:13:09 +0530

    List view on Transaction_Fragment added

--- app/src/main/java/com/example/financialapp/Transaction_Fragment.java
+++ app/src/main/java/com/example/financialapp/Transaction_Fragment.java
@@ -1,5 +1,8 @@
 package com.example.financialapp;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,12 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Transaction_Fragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    EditText datePicker;
+
+    DatePickerDialog.OnDateSetListener onDateSetListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,14 +35,36 @@ public class Transaction_Fragment extends Fragment implements AdapterView.OnItem
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transaction_, container, false);
 
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        datePicker = view.findViewById(R.id.startDate);
+
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getActivity(), android.R.style.Theme_Holo_Dialog_MinWidth,
+                        onDateSetListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = dayOfMonth+"/"+month+"/"+year;
+                datePicker.setText(date);
+            }
+        };
+
         // Spinner element
-        final Spinner spinner = (Spinner) view.findViewById(R.id.SP_startMonth);
-        final Spinner spinnerEnd = (Spinner) view.findViewById(R.id.SP_endMonth);
+        final ListView historylist = (ListView) view.findViewById(R.id.transactionHistory);
 
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
-
-        // Spinner Drop down elements
+        // List elements
         List<String> categories = new ArrayList<String>();
         categories.add("January");
         categories.add("February");
@@ -48,14 +80,13 @@ public class Transaction_Fragment extends Fragment implements AdapterView.OnItem
         categories.add("December");
 
         // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, categories);
 
         // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
-        spinnerEnd.setAdapter(dataAdapter);
+        historylist.setAdapter(dataAdapter);
 
         return view;
     }

dc209f45556e191130f42b9bddae2c03adf9b102
Ashen Silva <ashensilva96@gmail.com>
2022-06-23 10:36:32 +0530

    Dashboard, Transaction, Profile Fragments added.

--- app/src/main/java/com/example/financialapp/Transaction_Fragment.java
+++ app/src/main/java/com/example/financialapp/Transaction_Fragment.java
@@ -1,64 +1,72 @@
 package com.example.financialapp;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Transaction_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Transaction_Fragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Transaction_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Transaction_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Transaction_Fragment newInstance(String param1, String param2) {
-        Transaction_Fragment fragment = new Transaction_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class Transaction_Fragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction_, container, false);
+        View view = inflater.inflate(R.layout.fragment_transaction_, container, false);
+
+        // Spinner element
+        final Spinner spinner = (Spinner) view.findViewById(R.id.SP_startMonth);
+        final Spinner spinnerEnd = (Spinner) view.findViewById(R.id.SP_endMonth);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("January");
+        categories.add("February");
+        categories.add("March");
+        categories.add("April");
+        categories.add("May");
+        categories.add("June");
+        categories.add("July");
+        categories.add("August");
+        categories.add("September");
+        categories.add("October");
+        categories.add("November");
+        categories.add("December");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        spinnerEnd.setAdapter(dataAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
